cf0b807d5d0cc346a4ecc6402805b3bb48147a88
Administrator <Administrator@huiweilong>
2019-09-18 22:22:11 +0800

    --20190918  格式调整

--- src/main/java/com/huiwl/util/proxy/MyProxy.java
+++ src/main/java/com/huiwl/util/proxy/MyProxy.java
@@ -18,22 +18,20 @@ public class MyProxy {
 		Class<?>[] interfaces = aopService.getClass().getInterfaces();
 		// 方法执行器
 		InvocationHandler h = new InvocationHandler() {
-			
+
 			/**
-			 * Object proxy：代理对象
-			 * Method method：目标方法
-			 * Object[] args：目标方法的参数
+			 * Object proxy：代理对象 Method method：目标方法 Object[] args：目标方法的参数
 			 */
 			@Override
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-				
+
 				System.out.println("JDK动态代理");
 				Object result = method.invoke(aopService, null);
 				return result;
 			}
 		};
 		Object proxy = Proxy.newProxyInstance(loader, interfaces, h);
-		return (AopService)proxy;
+		return (AopService) proxy;
 
 	}
 
