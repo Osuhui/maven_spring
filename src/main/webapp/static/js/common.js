9310e63dd5cfffa1863fee2e0b495769a6ac1d9e
huiweilong <huiweilong@huiweilong.D2.HYRON.COM>
2019-09-23 17:58:46 +0900

    --20190923

---
+++ src/main/webapp/static/js/common.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	var center_div = document.getElementById("center_div");// 获取div块对象
+	var height_browser = document.documentElement.clientHeight;// 取得浏览器页面可视区域的宽度
+	var width_browser = document.documentElement.clientWidth;// 取得浏览器页面可视区域的宽度
+	var height_div = center_div.offsetHeight;// 获取div块的高度值
+	var width_div = center_div.offsetWidth;// 获取div块的宽度值
+	center_div.style.top = (height_browser - height_div) / 2 + "px";
+	center_div.style.left = (width_browser - width_div) / 2 + "px";
+
+}
